f0c3596511782224590a760c0b5f9941cf7e7716
ChiefOmnicron <eistonen.kodan.sasen@gmail.com>
2025-04-16 13:28:08 +0000

    Logging for job detection

--- event_worker/src/industry/job_detection.rs
+++ event_worker/src/industry/job_detection.rs
@@ -19,6 +19,7 @@ pub fn job_detection(
     used_ids:        &mut Vec<Uuid>,
     used_job_ids:    &mut Vec<JobId>,
 ) -> (HashMap<Uuid, Vec<UpdateJobRequest>>, Vec<IndustryJobEntry>) {
+    dbg!(&container_names);
     let now = chrono::Utc::now().naive_utc();
 
     // List of entries that need to be updated
@@ -74,14 +75,16 @@ pub fn job_detection(
         }
 
         // 4. Try to find the project based on its output location
+        dbg!(entry.output_location_id);
         if let Some(container) = container_names.get(&*entry.output_location_id) {
+            dbg!(container);
             if let Some(job) = startable_jobs
                 .iter()
                 .find(|x|
                     x.type_id == entry.product_type_id &&
                     x.runs == entry.runs &&
                     x.job_id.is_none() &&
-                    x.project_name == *container &&
+                    dbg!(x.project_name.clone()) == *container &&
                     !used_ids.contains(&x.id) &&
                     !used_job_ids.contains(&entry.job_id)
                 ) {
@@ -89,7 +92,7 @@ pub fn job_detection(
                         id:           job.id,
                         character_id: Some(entry.installer_id),
                         project_id:   Some(job.project_id),
-                        type_id:      entry.product_type_id,
+                        type_id:      dbg!(entry.product_type_id),
                         cost:         entry.cost,
                         status:       ProjectJobStatus::Building,
                         job_id:       Some(*entry.job_id),
