8e742a782915020f88536148fd7abfca75580a43
ChiefOmnicron <eistonen.kodan.sasen@gmail.com>
2025-05-30 22:45:54 +0000

    WIP

---
+++ webapp/src/services/project-group/list.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import type { Uuid } from "../utils";
+
+export const LIST_PROJECT_GROUPS = 'listProjectGroups';
+
+export const listProjectGroups = async (
+    filter: ProjectGroupFilter,
+): Promise<Uuid[]> => axios.get(
+        '/api/project-groups',
+        {
+            params: filter,
+        }
+    )
+    .then(x => x.data);
+
+export interface ProjectGroupFilter {
+
+}
