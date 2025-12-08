a037435b30e73fa4e6e83cb3944106bc1498b14c
ChiefOmnicron <eistonen.kodan.sasen@gmail.com>
2025-12-08 12:06:40 +0000

    WIP loading animation

--- webapp_components/loading_animation/generate.js
+++ webapp_components/loading_animation/generate.js
@@ -1,5 +1,6 @@
 const fs = require('node:fs');
-const map_data = require('./map.json')
+const map_data = require('./map.json');
+const { start } = require('node:repl');
 
 let xMin = 0;
 let xMax = 0;
@@ -42,11 +43,33 @@ const points = map_data
     .map(x => {
         const xPos = (x.position.x - xMin) / MAP_SIZE * IMAGE_SIZE;
         const yPos = (-(x.position.z - zMax)) / MAP_SIZE * IMAGE_SIZE;
-        return `<circle r="1" cx="${xPos}" cy="${yPos}" fill="black">${animation(x.region_id)}</circle>`
+        let color = '#9bb0ff';
+
+        // colors taken from wikipedia
+        // https://en.wikipedia.org/wiki/Stellar_classification
+        const temperature = x.star.temperature;
+        if (temperature >= 2300 && temperature <= 3900) {
+            color = '#ffcc6f'
+        } else if (temperature > 3900 && temperature <= 5300) {
+            color = '#ffd2a1'
+        } else if (temperature > 5300 && temperature <= 6000) {
+            color = '#fff4ea'
+        } else if (temperature > 6000 && temperature <= 7300) {
+            color = '#f8f7ff'
+        } else if (temperature > 7300 && temperature <= 10000) {
+            color = '#cad7ff'
+        } else if (temperature > 10000 && temperature <= 33000) {
+            color = '#aabfff'
+        } else {
+            color = '#9bb0ff'
+        }
+
+        return `<circle r="1" cx="${xPos}" cy="${yPos}" fill="${color}">${animation(x.region_id)}</circle>`
     });
 
 const content = `<svg
     xmlns="http://www.w3.org/2000/svg"
+    style="background-color: black"
     viewBox="0 0 512 512"
 >
     ${points.join('\n')}
